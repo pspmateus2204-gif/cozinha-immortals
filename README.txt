c644d677eb4c9629f0afe2fbf1cf40a4b6a9d247
pspmateus2204-gif <psp.mateus2204@gmail.com>
2026-09-24 09:07:32 -0300

    Add files via upload

--- README.txt
+++ README.txt
@@ -12,3 +12,7 @@ V5: adicionada a frase 'PNC DA WT' no canto direito do cabeçalho.
 V6: seleção diária de Chef Cedric/Rosa/Gene/Arwen; somente o chef escolhido expande os ingredientes e entra no cálculo das recomendações.
 
 V7: seletor de chef movido para o topo, imediatamente abaixo da escolha do dia, com destaque visual.
+
+V8: corrigido bug de renderização dos cards de ingredientes/chefs; cards dos 4 chefs agora aparecem e exibem os 2 ingredientes de cada um.
+
+V9: logo oficial da Immortals aplicado como identidade visual do site, ícone do app/PWA, favicon e imagem social.
--- sw.js
+++ sw.js
@@ -1,1 +1,5 @@
-const CACHE='slayer-dv-v3';const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg'];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE='cozinha-immortals-v9';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./immortals-logo.jpg','./icon-192.png','./icon-512.png','./apple-touch-icon.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
